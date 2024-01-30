0c63a2afcb3e7154e1f4106e5640410d12192590
o0pmitev <pmitev89@gmail.com>
2024-01-30 21:23:12 +0200

    fix: add user name input functionality

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [DefaultExecutionOrder(1000)]
-// TODO: Creat field promptin user for adding username:
-// TODO: The name should be displayed next to the score
+//TODO: Transfer the User name with the base score to the main scene 
 public class MenuUIHandler : MonoBehaviour
 {
+    public int score = 0;
+
+    private InputField userInput;
+    private TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        userInput = GameObject.Find("UserName").GetComponent<InputField>();
+
+        userInput.onEndEdit.AddListener(GetUserInput);
+
+        scoreText = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+    }
     public void StartNew()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void GetUserInput(string input)
+    {
+        scoreText.text ="Best Score: " + input + ": " + score;
     }
 
     public void Exit()
